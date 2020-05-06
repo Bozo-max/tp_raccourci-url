55e2fe2bb6f4eef3b98ded8d2ad59e8a4f017feb
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-06 12:09:20 +0200

    add admin part for mini_url

--- m/admin.py
+++ m/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MiniUrl
 
+class MiniUrlAdmin(admin.ModelAdmin):
+    list_display = ['url', 'code', 'pseudo', 'creation_date']
+    list_filter = ('pseudo',)
+    date_hierarchy = 'creation_date'
+    ordering = ('creation_date',)
+    search_fields = ('url',)
+
+admin.site.register(MiniUrl, MiniUrlAdmin)
 # Register your models here.
