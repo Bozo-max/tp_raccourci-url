c18c6e8bce1aedf16fd6d61d123af500fa392f85
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-06 11:02:13 +0200

    Home view, model definition, form definition and form view

--- m/models.py
+++ m/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+
+import random
+import string
+
+
+def generer(nb_caracters):
+    caracters = string.ascii_letters + string.digits
+    alea = [random.choice(caracters) for _ in range(nb_caracters)]
+    return alea
+
 
 # Create your models here.
+class MiniUrl(models.Model):
+    url = models.URLField(unique = True)
+    code = models.CharField(max_length = 6, unique = True)
+    creation_date = models.DateTimeField(default = timezone.now)
+    pseudo = models.CharField(max_length = 30)
+    nb_access = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.url
+
+    def save(self, *args, **kwargs):
+        code = generer(6)
+        super().save(*args, **kwargs)
--- m/views.py
+++ m/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import MiniUrl
+from .forms import MiniUrlForm
+
+def home(request):
+    mini_urls = MiniUrl.objects.all()
+    return render(request, 'm/home.html.twig', locals())
+
+def add_url(request):
+    form = MiniUrlForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    return render(request, 'm/url_add_form.html.twig', locals())
 
 # Create your views here.
